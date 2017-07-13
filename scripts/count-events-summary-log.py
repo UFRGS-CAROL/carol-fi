abaab0f9f812ffdcc3f13d409f01094927f031f4
Daniel Oliveira <dagoliveira@inf.ufrgs.br>
2017-07-13 11:38:15 -0300

    script to help count the events logged into summary log file

---
+++ scripts/count-events-summary-log.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import re
+from collections import Counter
+
+
+fp = open("summary-carolfi.log", "r")
+events = list()
+for line in fp:
+    m = re.match("(.*) - [Fault Injection Failed|Hang|SDC|NoOutputGenerated|Masked]",line)
+    if m:
+        line = line.strip()
+        events.append(line)
+
+print "FI attempts: ",len(l)
+for k,v in Counter(events).most_common():
+    print v,"\t",k
