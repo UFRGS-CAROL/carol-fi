b2be3dd7d61e84bdb6196236f9cb671f44404e5d
reliabilitylogdata <reliabilitylogdata@github.com>
2018-01-21 13:56:54 -0200

    profiler

--- profiler.py
+++ profiler.py
@@ -66,7 +66,7 @@ to fault injection
 def get_kernel_address_event(event):
     global KERNEL_INFO_LIST, KERNEL_INFO_DIR
     ev = vars(event)
-    print "Event info" , ev["breakpoints"]
+    print "Event info" , vars(ev["breakpoints"][0])
 
 """
 Set temporary breakpoints.
