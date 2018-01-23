0ae05d49600ac5258372b9875de95a9fb1aad2d9
Fernando <f_157@hotmail.com>
2018-01-23 18:11:42 -0200

    flip value

--- flip_value.py
+++ flip_value.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import gdb
 import sys
+import re
 
 sys.path.append() # I have to fix it
 import common_functions as cf # All common functions will be at common_functions module
@@ -11,6 +12,8 @@ CONF_LOCATION = "<conf-location>"
 VALID_BLOCK = None
 VALID_THREAD = None
 VALID_REGISTER = None
+BITS_TO_FLIP = None
+FAULT_MODEL = None
 
 """
 function called when the execution is stopped
@@ -19,27 +22,58 @@ function called when the execution is stopped
 
 def fault_injection(event):
     global VALID_BLOCK, VALID_THREAD, VALID_REGISTER
+    global BITS_TO_FLIP, FAULT_MODEL
     
     threadFocus = gdb.execute("cuda kernel 0 block " + str(VALID_BLOCK[0]) + "," + str(VALID_BLOCK[1]) + "," + str(VALID_BLOCK[2]) +
                 " thread " + str(VALID_THREAD[0]) + "," + str(VALID_THREAD[1]) + "," + str(VALID_THREAD[2]), to_string=True)
     
-    generic_injector(VALID_REGISTER, "bit_flip")
+    generic_injector(VALID_REGISTER, BITS_TO_FLIP, FAULT_MODEL)
+
+"""
+Flip only a bit in a register content
+"""
 
 
-def generic_injector(register, faultModel):
-    if faultModel == "bit_flip":
-        pass
-    elif faultModel == "multiple_bitflip":
-        pass
-    elif faultModel == "random_value":
-        pass
-    elif faultModel == "zero_value":
-        pass
+def flip_a_bit(bit_to_flip, reg_content):
+    new_bit = '0' if reg_content[bit_to_flip] == 1 else '1'
+    reg_content = reg_content[:bit_to_flip] + new_bit + reg_content[bit_to_flip - 1:]
+    return reg_content
+
+"""
+Flip a bit or multiple based on a fault model
+"""
 
 
+def generic_injector(register, bits_to_flip, fault_model):
+    # get register content
+    reg_cmd = cf.execute_command("p/t $" + str(register))
+    m = re.match("\$(\d+)[ ]*=[ ]*(\S+).*", reg_cmd)
+    if m:
+        reg_content = str(m.group(2))
 
+        if fault_model == "single":
+            # single bit flip
+            reg_content = flip_a_bit(bits_to_flip[0], reg_content)
 
+        elif fault_model == "multiple":
+            # multiple bit flip
+            for bit_to_flip in bits_to_flip:
+                reg_content = flip_a_bit(bit_to_flip, reg_content)
 
+        elif fault_model == "random":
+            # random value is stored at bits_to_flip[0]
+            reg_content = str(bits_to_flip[0])
+
+        elif fault_model == "zero":
+            reg_content = '0'
+
+        # send the new value to gdb
+        reg_cmd_flipped = cf.execute_command("set $" + str(register) + " = " + reg_content)
+
+    else:
+        raise NotImplementedError
+
+    return reg_cmd_flipped
 
 
 
@@ -57,11 +91,10 @@ try:
     gdbInitStrings = conf.get("DEFAULT", "gdbInitStrings")
 
     for initStr in gdbInitStrings.split(";"):
-        print initStr
         gdb.execute(initStr)
 
 except gdb.error as err:
-    print "initializing setup: " + str(err)
+    print ("initializing setup: " + str(err))
 
 
 
