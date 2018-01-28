4f247a79f90d3acf64138dbf2b0b3987ff741e81
reliabilitylogdata <reliabilitylogdata@github.com>
2018-01-28 20:55:24 -0200

    fault injection

--- flip_value.py
+++ flip_value.py
@@ -67,9 +67,9 @@ Flip a bit or multiple based on a fault model
 
 def generic_injector():
     global valid_register, bits_to_flip, fault_model
+    print valid_register
     # get register content
     reg_cmd = cf.execute_command(gdb, "p/t $" + str(valid_register))
-    print reg_cmd
 
     # Logging info result extracted from register
     logging.info("reg old value: " + str(reg_cmd[0]))
