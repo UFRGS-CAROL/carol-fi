f5f8a2bde523a7d0a4113209fbf8929a2a9cc552
Fernando <f_157@hotmail.com>
2018-03-09 17:28:24 -0300

    test

--- profiler.py
+++ profiler.py
@@ -16,7 +16,7 @@ necessary to fault injection
 
 def get_kernel_address_event(event):
     global kernel_info_list
-    print("KKKKKKKKKKK", event.breakpoints, kernel_info_list)
+
     # Search all kernels info, and all breakpoints
     for kernel_info in kernel_info_list:
         for breakpoint in event.breakpoints:
@@ -58,8 +58,6 @@ def set_breakpoints(kernel_conf_string):
 
             kernel_info_list.append(kernel_info)
 
-    gdb.events.stop.connect(get_kernel_address_event)
-
 """
 Main function
 """
@@ -85,6 +83,7 @@ def main():
     # Run app for the first time
     if time_profiler == 'True':
         set_breakpoints(kernel_conf_string)
+        gdb.events.stop.connect(get_kernel_address_event)
 
     gdb.execute("r")
 
