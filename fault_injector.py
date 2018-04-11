0e3d5f11a77097e42a4361a57b97b89c83f32ae9
Fernando <f_157@hotmail.com>
2018-04-11 15:03:25 -0300

    update

--- fault_injector.py
+++ fault_injector.py
@@ -438,6 +438,7 @@ def run_gdb_fault_injection(**kwargs):
     # Check if app stops execution (otherwise kill it after a time)
     # is_hang = finish(section=section, conf=conf, logging=logging, timestamp_start=timestamp_start,
     #                  end_time=end_signal, p=fi_process)
+    is_hang = False
 
     # Make sure process finish before trying to execute again
     fi_process.join()
