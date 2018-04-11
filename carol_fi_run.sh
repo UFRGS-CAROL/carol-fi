b219b1ceaf4d910f918a95bb9dd348051ce64cd6
Fernando <f_157@hotmail.com>
2018-04-11 14:00:24 -0300

    update

--- carol_fi_run.sh
+++ carol_fi_run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 
-        os.environ['GDB_EXE'] = self.__gdb_exe_name
-        os.environ['GDB_CAROLFI_FLAGS'] = '-n -batch -x'
-        os.environ['PYTHON_SCRIPT'] = self.__flip_script
 $GDB_EXE $GDB_CAROLFI_FLAGS $PYTHON_SCRIPT > out.txt 2> err.txt
 
