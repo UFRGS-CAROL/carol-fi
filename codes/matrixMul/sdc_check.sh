13df7a2675b129ff45b92e62f05c7344945cbfde
Fernando <f_157@hotmail.com>
2018-04-11 09:01:11 -0300

    update

--- codes/matrixMul/sdc_check.sh
+++ codes/matrixMul/sdc_check.sh
@@ -4,4 +4,5 @@
 # Must compare all things here
 # Copied from SASSIFI
 # APP, GOLD_OUTPUT_PATH and INJ_OUTPUT_PATH are set on python script
+echo diff ${GOLD_OUTPUT_PATH} ${INJ_OUTPUT_PATH} > /tmp/diff_${APP}.log
 diff ${GOLD_OUTPUT_PATH} ${INJ_OUTPUT_PATH} > /tmp/diff_${APP}.log
