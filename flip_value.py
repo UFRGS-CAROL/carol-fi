62ce99ca572430b59ef5839751063813d4db52c6
Fernando <f_157@hotmail.com>
2018-04-11 15:01:22 -0300

    update

--- flip_value.py
+++ flip_value.py
@@ -223,6 +223,7 @@ def main():
     gdb.execute("c")
     print("depois do continue\n\n\n")
     breakpoint_kernel_address.delete()
+    print("depois do delete \n\n\n")
 
 global_valid_block, global_valid_thread, global_bits_to_flip = [None] * 3
 global_fault_model, global_valid_register, global_logging = [None] * 3
