e60262742f119cd20f66c6328ebfb4d19661b64c
Fernando <f_157@hotmail.com>
2018-04-11 15:33:05 -0300

    update

--- codes/matrixMul/sdc_check.sh
+++ codes/matrixMul/sdc_check.sh
@@ -1,30 +1,27 @@
 # Remove GDB specific outputs from gold and inj output
 CLEAN_GOLD=/tmp/clean_carol_fi_gold.txt
 
-sed '/\[New Thread/d' $GOLD_OUTPUT_PATH > $CLEAN_GOLD
-sed '/\[Thread/d' $CLEAN_GOLD > $CLEAN_GOLD
-sed '/\[Switching/d' $CLEAN_GOLD > $CLEAN_GOLD
-sed '/\[Inferior/d' $CLEAN_GOLD > $CLEAN_GOLD
-
 # Remove breakpoint and folowing line
-sed -e '/Breakpoint/,+2d' $CLEAN_GOLD > $CLEAN_GOLD
+sed -e '/Breakpoint/,+2d' ${GOLD_OUTPUT_PATH} > ${CLEAN_GOLD}
+
+sed '/\[New Thread/d' ${CLEAN_GOLD} > ${CLEAN_GOLD}
+sed '/\[Thread/d' ${CLEAN_GOLD} > ${CLEAN_GOLD}
+sed '/\[Switching/d' ${CLEAN_GOLD} > ${CLEAN_GOLD}
+sed '/\[Inferior/d' ${CLEAN_GOLD} > ${CLEAN_GOLD}
 
 # Remove inj output
 CLEAN_INJ_OUTPUT=/tmp/clean_carol_fi_inj.txt
-sed '/\[New Thread/d' $INJ_OUTPUT_PATH > $CLEAN_INJ_OUTPUT
-sed '/\[Thread/d' $CLEAN_INJ_OUTPUT > $CLEAN_INJ_OUTPUT
-sed '/\[Switching/d' $CLEAN_INJ_OUTPUT > $CLEAN_INJ_OUTPUT
-sed '/\[Inferior/d' $CLEAN_INJ_OUTPUT > $CLEAN_INJ_OUTPUT
-
 # Remove breakpoint and folowing line
-sed -e '/Breakpoint/,+2d' $CLEAN_INJ_OUTPUT > $CLEAN_INJ_OUTPUT
-
-
+sed -e '/Breakpoint/,+2d' ${INJ_OUTPUT_PATH} > ${CLEAN_INJ_OUTPUT}
+sed '/\[New Thread/d' ${CLEAN_INJ_OUTPUT} > ${CLEAN_INJ_OUTPUT}
+sed '/\[Thread/d' ${CLEAN_INJ_OUTPUT} > ${CLEAN_INJ_OUTPUT}
+sed '/\[Switching/d' ${CLEAN_INJ_OUTPUT} > ${CLEAN_INJ_OUTPUT}
+sed '/\[Inferior/d' ${CLEAN_INJ_OUTPUT} > ${CLEAN_INJ_OUTPUT}
 
 # SDC checking diff
 # Must compare all things here
 # Copied from SASSIFI
 # APP, GOLD_OUTPUT_PATH and INJ_OUTPUT_PATH are set on python script
-diff $CLEAN_GOLD $CLEAN_INJ_OUTPUT > $DIFF_LOG
+diff ${CLEAN_GOLD} ${CLEAN_INJ_OUTPUT} > $DIFF_LOG
 
 #rm -f /tmp/${CLEAN_GOLD} /tmp/${CLEAN_INJ_OUTPUT}
