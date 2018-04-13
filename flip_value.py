c3451a804b1847321cff51f6d324ce920274eb9e
Fernando <f_157@hotmail.com>
2018-04-13 17:19:25 -0300

    flip

--- flip_value.py
+++ flip_value.py
@@ -65,7 +65,7 @@ def fault_injection_breakpoint(event):
     # a normal breakpoint is hit
     global_logging.debug("Trying Fault Injection")
     inferior = gdb.selected_inferior()
-    print(vars(inferior))
+    print(dir(inferior))
     threadsSymbols = []
     for th in inferior.threads():
         try:
