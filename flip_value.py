02378ffd0b7d499487f792adb76540dfe76f0756
reliabilitylogdata <reliabilitylogdata@github.com>
2018-04-15 20:23:54 -0300

    flip value

--- flip_value.py
+++ flip_value.py
@@ -6,6 +6,7 @@ import os
 import common_functions as cf  # All common functions will be at common_functions module
 import common_parameters
 
+
 class Breakpoint(gdb.Breakpoint):
     # def __init__(self):
     #     super(Breakpoint, self).__init__()
@@ -13,8 +14,8 @@ class Breakpoint(gdb.Breakpoint):
     #     global global_bits_to_flip, global_fault_model, global_logging
     #     global ready_to_inject
 
-        # if not ready_to_inject:
-        #     return
+    # if not ready_to_inject:
+    #     return
 
     def stop(self):
         # This if avoid the creation of another event connection
@@ -25,6 +26,7 @@ class Breakpoint(gdb.Breakpoint):
         frame = gdb.selected_frame()
         block = frame.block()
         names = set()
+        symbols = []
         while block:
             if (block.is_global):
                 print()
@@ -34,75 +36,260 @@ class Breakpoint(gdb.Breakpoint):
                     name = symbol.name
                     if not name in names:
                         # print('{} = {}'.format(name, symbol.value(frame)))
-                        print(symbol.name, symbol.is_variable, symbol.value, symbol.addr_class,
-                              symbol.line, symbol.value(frame).type.strip_typedefs().code)
-
+                        # print(symbol.name, symbol.is_variable, symbol.value, symbol.addr_class,
+                        #       symbol.line, symbol.value(frame).type.strip_typedefs().code)
+                        symbols.append(symbol)
                         names.add(name)
             block = block.superblock
 
+        pos = random.randint(0, len(symbols))
+        bitFlipValue(symbols[pos])
         return True
 
 
+def genericBitFlip(value):
+    global global_fault_model
+    tag = "genericBitFlip"
+    bufLog = ""
+
+    address = re.sub("<.*>|\".*\"", "", str(value.address))
+    byteSizeof = value.type.strip_typedefs().sizeof
+    bufLog += "Memory content before bitflip:" + str(showMemoryContent(address, byteSizeof))
+    bufLog += "\n"
+    choice = int(global_fault_model)
+    if choice == 0:
+        bufLog += singleBitFlipWordAddress(address, byteSizeof)
+    elif choice == 1:
+        bufLog += doubleBitFlipWordAddress(address, byteSizeof)
+    elif choice == 2:
+        bufLog += randomBitFlipWordAddress(address, byteSizeof)
+    elif choice == 3:
+        bufLog += zeroBitFlipWordAddress(address, byteSizeof)
+    elif choice == 4:
+        bufLog += LSBFlipWordAddress(address, byteSizeof)
+    bufLog += "Memory content after  bitflip:" + str(showMemoryContent(address, byteSizeof))
+    bufLog += "\n"
+    return bufLog
 
 
-
-"""
-function called when the execution is stopped by a breakpoint
-"""
-
-
-def fault_injection_breakpoint(event):
-    global global_valid_block, global_valid_thread, global_valid_register
-    global global_bits_to_flip, global_fault_model, global_logging
-    global ready_to_inject
-
-    # if not ready_to_inject:
-    #     return
-
-    # This if avoid the creation of another event connection
-    # for some reason gdb cannot breakpoint addresses before
-    # a normal breakpoint is hit
-    global_logging.debug("Trying Fault Injection")
-    inferior = gdb.selected_inferior()
-
-    threadsSymbols = []
-
-    for th in inferior.threads():
-        print(dir(th))
-        try:
-            th.switch()
-            thSymbols = getAllValidSymbols()
-            if len(thSymbols) > 0:
-                threadsSymbols.append([th, thSymbols])
-        except:
-            continue
+def LSBFlipWordAddress(address, byteSizeof):
+    tag = "LSBBitFlip"
+    bufLog = ""
+    bufLog += "Fault Model: LSB bit-flip"
+    bufLog += "\n"
+    bufLog += "base address to flip value: " + str(address)
+    bufLog += "\n"
+    bufLog += "address max offset: " + str(byteSizeof)
+    bufLog += "\n"
+    addressOffset = byteSizeof - 1  # get the least significant byte only
+    addressF = hex(int(address, 16) + addressOffset)
+    xMem = "x/1tb " + str(addressF)
+    binData = gdb.execute(xMem, to_string=True)
+    binData = re.sub(".*:|\s", "", binData)
+    binDatal = list(binData)
+    bitPos = random.randint(0, len(binDatal) - 1)
+    if binDatal[bitPos] == '1':
+        binDatal[bitPos] = '0'
+    else:
+        binDatal[bitPos] = '1'
+    binData = ''.join(binDatal)
+    setCmd = "set {char}" + addressF + " = " + hex(int(binData, 2))
+    gdb.execute(setCmd)
+    return bufLog
 
 
-            # try:
-            #     change_focus_cmd = "cuda kernel 0 block {0},{1},{2} thread {3},{4},{5}".format(str(global_valid_block[0]),
-            #                                                                                    str(global_valid_block[1]),
-            #                                                                                    str(global_valid_block[2]),
-            #                                                                                    str(global_valid_thread[0]),
-            #                                                                                    str(global_valid_thread[1]),
-            #                                                                                    str(global_valid_thread[2]))
-            #     thread_focus = gdb.execute(change_focus_cmd, to_string=True)
-            #
-            # except Exception as err:
-            #     global_logging.exception("CUDA_FOCUS_exception: " + str(err))
-            #     global_logging.exception("Fault Injection Went Wrong")
-            #     return
-            #
-            # try:
-            #     # Thread focus return information
-            #     global_logging.info(str(thread_focus).replace("[", "").replace("]", "").strip())
-            #
-            #     # Do the fault injection magic
-            #     generic_injector(global_valid_register, global_bits_to_flip, global_fault_model)
-            #
-            #     global_logging.info("Fault Injection Successful")
-            # except Exception as err:
-            #     global_logging.exception("fault_injection_python_exception: " + str(err))
-            #     global_logging.exception("Fault Injection Went Wrong")
+def bitFlipValue(value):
+    tag = "bitFlipValue"
+    bufLog = ""
+    if value.type.strip_typedefs().code is gdb.TYPE_CODE_PTR:
+        random.seed()
+        pointerFlip = random.randint(0, 1)
+        pointedAddress = re.sub("<.*>|\".*\"", "", str(value.referenced_value().address))
+        if pointerFlip or hex(int(pointedAddress, 16)) <= hex(int("0x0", 16)):
+            bufLog += str("Fliping a bit of the pointer")
+            bufLog += "\n"
+            bufLog += genericBitFlip(value)
+        else:
+            bufLog += str("Fliping a bit of the value pointed by a pointer")
+            bufLog += "\n"
+            bufLog += bitFlipValue(value.referenced_value())
+    elif value.type.strip_typedefs().code is gdb.TYPE_CODE_REF:
+        random.seed()
+        refFlip = random.randint(0, 1)
+        pointedAddress = re.sub("<.*>|\".*\"", "", str(value.referenced_value().address))
+        if refFlip or hex(int(pointedAddress, 16)) <= hex(int("0x0", 16)):
+            bufLog += str("Fliping a bit of the reference")
+            bufLog += "\n"
+            bufLog += genericBitFlip(value)
+        else:
+            bufLog += str("Fliping a bit of the value pointed by a reference")
+            bufLog += "\n"
+            bufLog += bitFlipValue(value.referenced_value())
+    elif value.type.strip_typedefs().code is gdb.TYPE_CODE_ARRAY:
+        rangeMax = value.type.strip_typedefs().range()[1]
+        random.seed()
+        arrayPos = random.randint(0, rangeMax)
+        bufLog += "Fliping array at pos: " + str(arrayPos)
+        bufLog += "\n"
+        bufLog += bitFlipValue(value[arrayPos])
+    elif value.type.strip_typedefs().code is gdb.TYPE_CODE_STRUCT:
+        fields = value.type.fields()
+        if not fields:
+            bufLog += genericBitFlip(value)
+        random.seed()
+        fieldPos = random.randint(0, len(fields) - 1)
+        newValue = value[fields[fieldPos]]
+        count = 0
+        newAddress = re.sub("<.*>|\".*\"", "", str(newValue.address))
+        while newValue.address is None or newValue.is_optimized_out or hex(int(newAddress, 16)) <= hex(int("0x0", 16)):
+            random.seed()
+            fieldPos = random.randint(0, len(fields) - 1)
+            newValue = value[fields[fieldPos]]
+            newAddress = re.sub("<.*>|\".*\"", "", str(newValue.address))
+            count += 1
+            if count == 20:
+                raise Exception("Unable to exit loop in struct fields; Exiting wihtout making a bit flip")
+
+        bufLog += "Fliping value of field: " + str(fields[fieldPos].name)
+        bufLog += "\n"
+        bufLog += bitFlipValue(newValue)
+    elif value.type.strip_typedefs().code is gdb.TYPE_CODE_UNION:
+        fields = value.type.fields()
+        random.seed()
+        fieldPos = random.randint(0, len(fields) - 1)
+        newValue = value[fields[fieldPos]]
+        count = 0
+        newAddress = re.sub("<.*>|\".*\"", "", str(newValue.address))
+        while newValue.address is None or newValue.is_optimized_out or hex(int(newAddress, 16)) <= hex(int("0x0", 16)):
+            random.seed()
+            fieldPos = random.randint(0, len(fields) - 1)
+            newValue = value[fields[fieldPos]]
+            count += 1
+            if count == 20:
+                # logMsg(str("Error: unable to exit loop in union fields; Exiting wihtout making bitflip"))
+                # return
+                raise Exception("Unable to exit loop in union fields; Exiting wihtout making bitflip")
+
+        bufLog += "Fliping value of field name: " + str(fields[fieldPos].name)
+        bufLog += "\n"
+        bufLog += bitFlipValue(newValue)
+    else:
+        bufLog += genericBitFlip(value)
+
+    return bufLog
+
+
+def singleBitFlipWordAddress(address, byteSizeof):
+    tag = "singleBitFlip"
+    bufLog = ""
+    bufLog += "Fault Model: Single bit-flip"
+    bufLog += "\n"
+    bufLog += "base address to flip value: " + str(address)
+    bufLog += "\n"
+    bufLog += "address max offset: " + str(byteSizeof)
+    bufLog += "\n"
+    random.seed()
+    addressOffset = random.randint(0, byteSizeof - 1)
+    addressF = hex(int(address, 16) + addressOffset)
+    xMem = "x/1tb " + str(addressF)
+    binData = gdb.execute(xMem, to_string=True)
+    binData = re.sub(".*:|\s", "", binData)
+    binDatal = list(binData)
+    bitPos = random.randint(0, len(binDatal) - 1)
+    if binDatal[bitPos] == '1':
+        binDatal[bitPos] = '0'
+    else:
+        binDatal[bitPos] = '1'
+    binData = ''.join(binDatal)
+    setCmd = "set {char}" + addressF + " = " + hex(int(binData, 2))
+    gdb.execute(setCmd)
+    return bufLog
+
+
+def doubleBitFlipWordAddress(address, byteSizeof):
+    tag = "doubleBitFlip"
+    bufLog = ""
+    bufLog += "Fault Model: Double bit-flip"
+    bufLog += "\n"
+    bufLog += "base address to flip value: " + str(address)
+    bufLog += "\n"
+    bufLog += "address max offset: " + str(byteSizeof)
+    bufLog += "\n"
+    random.seed()
+    addressOffset = random.randint(0, byteSizeof - 1)
+    addressF = hex(int(address, 16) + addressOffset)
+    xMem = "x/1tb " + str(addressF)
+    binData = gdb.execute(xMem, to_string=True)
+    binData = re.sub(".*:|\s", "", binData)
+    binDatal = list(binData)
+    bitPos = random.randint(0, len(binDatal) - 1)
+    bitPos2 = random.randint(0, len(binDatal) - 1)
+    while bitPos == bitPos2:
+        bitPos2 = random.randint(0, len(binDatal) - 1)
+    if binDatal[bitPos] == '1':
+        binDatal[bitPos] = '0'
+    else:
+        binDatal[bitPos] = '1'
+    if binDatal[bitPos2] == '1':
+        binDatal[bitPos2] = '0'
+    else:
+        binDatal[bitPos2] = '1'
+    binData = ''.join(binDatal)
+    setCmd = "set {char}" + addressF + " = " + hex(int(binData, 2))
+    gdb.execute(setCmd)
+    return bufLog
+
+
+def randomBitFlipWordAddress(address, byteSizeof):
+    tag = "randomBitFlip"
+    bufLog = ""
+    bufLog += "Fault Model: Random bit-flip"
+    bufLog += "\n"
+    bufLog += "base address to flip value: " + str(address)
+    bufLog += "\n"
+    bufLog += "address max offset: " + str(byteSizeof)
+    bufLog += "\n"
+    random.seed()
+    addressOffset = 0
+    while addressOffset < byteSizeof:
+        addressF = hex(int(address, 16) + addressOffset)
+        xMem = "x/1tb " + str(addressF)
+        binData = gdb.execute(xMem, to_string=True)
+        binData = re.sub(".*:|\s", "", binData)
+        binDatal = list(binData)
+        for i in range(0, len(binDatal)):
+            binDatal[i] = str(random.randint(0, 1))
+        binData = ''.join(binDatal)
+        setCmd = "set {char}" + addressF + " = " + hex(int(binData, 2))
+        gdb.execute(setCmd)
+        addressOffset += 1
+    return bufLog
+
+
+def zeroBitFlipWordAddress(address, byteSizeof):
+    tag = "zeroBitFlip"
+    bufLog = ""
+    bufLog += "Fault Model: Zero bit-flip"
+    bufLog += "\n"
+    bufLog += "base address to flip value: " + str(address)
+    bufLog += "\n"
+    bufLog += "address max offset: " + str(byteSizeof)
+    bufLog += "\n"
+    random.seed()
+    addressOffset = 0
+    while addressOffset < byteSizeof:
+        addressF = hex(int(address, 16) + addressOffset)
+        xMem = "x/1tb " + str(addressF)
+        binData = gdb.execute(xMem, to_string=True)
+        binData = re.sub(".*:|\s", "", binData)
+        binDatal = list(binData)
+        for i in range(0, len(binDatal)):
+            binDatal[i] = '0'
+        binData = ''.join(binDatal)
+        setCmd = "set {char}" + addressF + " = " + hex(int(binData, 2))
+        gdb.execute(setCmd)
+        addressOffset += 1
+    return bufLog
 
 
 """
@@ -178,6 +365,13 @@ def generic_injector(valid_register, bits_to_flip, fault_model):
         return reg_content_old != reg_content_new
     else:
         raise NotImplementedError
+
+
+def showMemoryContent(address, byteSizeof):
+    xMem = "x/" + str(byteSizeof) + "xb " + address
+    hexData = gdb.execute(xMem, to_string=True)
+    hexData = re.sub(".*:|\s", "", hexData)
+    return hexData
 
 
 """
@@ -294,3 +488,63 @@ ready_to_inject = False
 
 # Call main execution
 main()
+
+
+# """
+# function called when the execution is stopped by a breakpoint
+# """
+#
+#
+# def fault_injection_breakpoint(event):
+#     global global_valid_block, global_valid_thread, global_valid_register
+#     global global_bits_to_flip, global_fault_model, global_logging
+#     global ready_to_inject
+#
+#     # if not ready_to_inject:
+#     #     return
+#
+#     # This if avoid the creation of another event connection
+#     # for some reason gdb cannot breakpoint addresses before
+#     # a normal breakpoint is hit
+#     global_logging.debug("Trying Fault Injection")
+#     inferior = gdb.selected_inferior()
+#
+#     threadsSymbols = []
+#
+#     for th in inferior.threads():
+#         print(dir(th))
+#         try:
+#             th.switch()
+#             thSymbols = getAllValidSymbols()
+#             if len(thSymbols) > 0:
+#                 threadsSymbols.append([th, thSymbols])
+#         except:
+#             continue
+#
+#
+#             # try:
+#             #     change_focus_cmd = "cuda kernel 0 block {0},{1},{2} thread {3},{4},{5}".format(str(global_valid_block[0]),
+#             #                                                                                    str(global_valid_block[1]),
+#             #                                                                                    str(global_valid_block[2]),
+#             #                                                                                    str(global_valid_thread[0]),
+#             #                                                                                    str(global_valid_thread[1]),
+#             #                                                                                    str(global_valid_thread[2]))
+#             #     thread_focus = gdb.execute(change_focus_cmd, to_string=True)
+#             #
+#             # except Exception as err:
+#             #     global_logging.exception("CUDA_FOCUS_exception: " + str(err))
+#             #     global_logging.exception("Fault Injection Went Wrong")
+#             #     return
+#             #
+#             # try:
+#             #     # Thread focus return information
+#             #     global_logging.info(str(thread_focus).replace("[", "").replace("]", "").strip())
+#             #
+#             #     # Do the fault injection magic
+#             #     generic_injector(global_valid_register, global_bits_to_flip, global_fault_model)
+#             #
+#             #     global_logging.info("Fault Injection Successful")
+#             # except Exception as err:
+#             #     global_logging.exception("fault_injection_python_exception: " + str(err))
+#             #     global_logging.exception("Fault Injection Went Wrong")
+#
