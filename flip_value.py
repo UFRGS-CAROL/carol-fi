217b0499ce1ebe4b0abc88665581fa03a4ce3dc0
reliabilitylogdata <reliabilitylogdata@github.com>
2018-04-15 18:04:57 -0300

    flip value

--- flip_value.py
+++ flip_value.py
@@ -41,7 +41,7 @@ def chooseFrameFlip(frameSymbols):
         # symbol = symbols[symbolPos]
         # varGDB = symbol.value(frame)
         for t in frameSymbols:
-            print(t)
+            print(t[1])
             # for symbol_i in t[1]:
             #     print(symbol_i.type, symbol_i.name, symbol_i.line,
             #           symbol_i.print_name, symbol_i.is_variable, symbol_i.value)
