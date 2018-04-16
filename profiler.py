3cbfa6a47952f64a4708eb842242ef3c9b542984
reliabilitylogdata <reliabilitylogdata@github.com>
2018-04-15 21:43:14 -0300

    flip value

--- profiler.py
+++ profiler.py
@@ -76,7 +76,7 @@ def main():
 
     # Initialize GDB to run the app
     gdb.execute("set confirm off")
-    gdb.execute("set pagination off")
+    # gdb.execute("set pagination off")
     # gdb_init_strings, kernel_conf_string, time_profiler = str(os.environ["CAROL_FI_INFO"]).split("|")
     # print(gdb_init_strings)
     gdb_init_strings = "file /home/carol/carol-fi/codes/matrixMul/matrixMul;"
