755ca0b1bc896107fd68787c49adeadbec8a8293
Fernando <f_157@hotmail.com>
2018-07-20 16:26:40 -0300

    flip value

--- fault_injector.py
+++ fault_injector.py
@@ -519,6 +519,7 @@ and the thread
 def get_valid_thread(threads):
     element = random.randrange(2, len(threads))
     #  (15,2,0) (31,12,0)    (15,2,0) (31,31,0)    20 0x0000000000b41a28 matrixMul.cu    47
+    print(threads[element])
     split = threads[element].replace("\n", "").split()
 
     # randomly chosen first block and thread
