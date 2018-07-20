8adb093e9dfb028c011596c22aa8bb62eaa41c2a
Fernando <f_157@hotmail.com>
2018-07-20 15:49:30 -0300

    flip value

--- fault_injector.py
+++ fault_injector.py
@@ -311,7 +311,6 @@ def check_sdcs_and_app_crash(logging, sdc_check_script):
         # Test if files are ok
         with open(cp.DIFF_LOG, 'r') as fi:
             out_lines = fi.readlines()
-            print(out_lines)
             if len(out_lines) != 0:
                 # Check if NVIDIA signals on output
                 for signal in cp.SIGNALS:
@@ -326,7 +325,6 @@ def check_sdcs_and_app_crash(logging, sdc_check_script):
 
         with open(cp.DIFF_ERR_LOG, 'r') as fi_err:
             err_lines = fi_err.readlines()
-            print(err_lines)
             if len(err_lines) != 0:
                 is_app_crash = True
 
@@ -777,11 +775,8 @@ def main():
     print("###################################################")
     # Clear /tmp files generated
     os.system("rm -f /tmp/carol-fi-kernel-info.txt")
-    # os.system("rm -f " + cp.GOLD_OUTPUT_PATH)
-    # os.system("rm -f " + cp.INJ_OUTPUT_PATH)
-    # os.system("rm -f " + cp.GOLD_ERR_PATH)
-    # os.system("rm -f " + cp.INJ_ERR_PATH)
-    # os.system("rm -f " + cp.DIFF_ERR_LOG + " " + cp.DIFF_LOG)
+    os.system("rm -f " + cp.GOLD_OUTPUT_PATH)
+    os.system("rm -f " + cp.GOLD_ERR_PATH)
     ########################################################################
 
 
