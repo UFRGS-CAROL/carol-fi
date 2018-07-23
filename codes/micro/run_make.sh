c5eb90d5490e701f3e1fa0089c6cdfa9d83b5ec5
carol <fernandofernandesantos@gmail.com>
2018-07-23 02:34:06 +0000

    register count

--- codes/micro/run_make.sh
+++ codes/micro/run_make.sh
@@ -2,8 +2,8 @@ for j in FMA ADD MUL;
 do 
 	for i in HALF SINGLE DOUBLE;
 	do
-		make TYPE=$j PRECISION=$i DEBUG=1 OPS=100 clean
-		make TYPE=$j PRECISION=$i DEBUG=1 OPS=100 generate
+		#make TYPE=$j PRECISION=$i DEBUG=1 OPS=100 clean
+		#make TYPE=$j PRECISION=$i DEBUG=1 OPS=100 generate
 		make TYPE=$j PRECISION=$i DEBUG=1 OPS=100 test
 	done;
 
