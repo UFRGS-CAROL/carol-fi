e40801e28804f72d58fcc62a1575572b926b9bf8
Fernando <f_157@hotmail.com>
2018-08-09 17:40:02 -0300

    profiler debug

--- fault_injector.py
+++ fault_injector.py
@@ -23,7 +23,8 @@ It makes standart gdb script calls
 
 def run_gdb_python(gdb_name, script):
     cmd = 'env CUDA_DEVICE_WAITS_ON_EXCEPTION=1 ' + gdb_name
-    cmd += ' -n --nh --nx -q --return-child-result -x ' + script  # -batch-silent
+    cmd += ' -n -batch -x ' + script  # -batch-silent
+    #-n --nh --nx -q --return-child-result -x 
     return cmd
 
 
