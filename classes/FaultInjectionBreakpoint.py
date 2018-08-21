02f3b899de0c64aa2bcacb5c26f4b1797dd1c8af
Fernando <f_157@hotmail.com>
2018-08-21 14:46:43 -0300

    fault injection

--- classes/FaultInjectionBreakpoint.py
+++ classes/FaultInjectionBreakpoint.py
@@ -357,8 +357,9 @@ class FaultInjectionBreakpoint(gdb.Breakpoint):
 
             print("Backtrace BEGIN:")
             bt = gdb.execute("bt", to_string=True)
+            print(bt)
             source_lines = gdb.execute("list", to_string=True)
-            print(bt, source_lines)
+            print(source_lines)
 
             threads_symbols = list()
             for th in inferior.threads():
