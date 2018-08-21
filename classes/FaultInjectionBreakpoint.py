fcd539d5fe10cc37c213c5707d3c8343080a3fe5
Fernando <f_157@hotmail.com>
2018-08-21 15:09:54 -0300

    fault injection

--- classes/FaultInjectionBreakpoint.py
+++ classes/FaultInjectionBreakpoint.py
@@ -374,32 +374,32 @@ class FaultInjectionBreakpoint(gdb.Breakpoint):
                 print(err)
                 print("ERROR ON THREAD INFERIORS")
                 continue
-        th_len = len(threads_symbols)
-        print("AFTER THREAD LEN")
-        if th_len <= 0:
-            print(str("No Threads with symbols"))
-            return False
-
-        th_pos = random.randint(0, th_len - 1)
-        print("AFTER THREAD rand int")
-        cur_thread = threads_symbols[th_pos][0]
-        print("Thread name: " + str(cur_thread.name))
-        print("Thread num: " + str(cur_thread.num))
-        print("Thread ptid: " + str(cur_thread.ptid))
-        r = self.__chose_frame_to_flip(threads_symbols[th_pos][1])
-        while r is False:
-            threads_symbols.pop(th_pos)
-            th_len -= 1
-            if th_len <= 0:
-                break
-            th_pos = random.randint(0, th_len - 1)
-            try:
-                r = self.__chose_frame_to_flip(threads_symbols[th_pos][1])
-            except Exception as err:
-                print(err)
-                r = False
-
-        return r
+        # th_len = len(threads_symbols)
+        # print("AFTER THREAD LEN")
+        # if th_len <= 0:
+        #     print(str("No Threads with symbols"))
+        #     return False
+        #
+        # th_pos = random.randint(0, th_len - 1)
+        # print("AFTER THREAD rand int")
+        # cur_thread = threads_symbols[th_pos][0]
+        # print("Thread name: " + str(cur_thread.name))
+        # print("Thread num: " + str(cur_thread.num))
+        # print("Thread ptid: " + str(cur_thread.ptid))
+        # r = self.__chose_frame_to_flip(threads_symbols[th_pos][1])
+        # while r is False:
+        #     threads_symbols.pop(th_pos)
+        #     th_len -= 1
+        #     if th_len <= 0:
+        #         break
+        #     th_pos = random.randint(0, th_len - 1)
+        #     try:
+        #         r = self.__chose_frame_to_flip(threads_symbols[th_pos][1])
+        #     except Exception as err:
+        #         print(err)
+        #         r = False
+        #
+        # return r
 
     """
     Get all the symbols of the stacked frames, returns a list of tuples [frame, symbolsList]
