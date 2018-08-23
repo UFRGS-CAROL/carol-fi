9ee9e3cd5affb95af2b164ca9cab6975cc73bada
Fernando <f_157@hotmail.com>
2018-08-23 17:09:55 -0300

    fault injection

--- flip_value.py
+++ flip_value.py
@@ -111,7 +111,7 @@ def main():
     gdb.execute("r")
     print("Passou 4")
 
-    while was_hit is False and 'This program' not in gdb.execute('c', to_string=True):
+    while was_hit is not False and 'This program' not in gdb.execute('c', to_string=True):
         pass
 
     # Delete the breakpoint
