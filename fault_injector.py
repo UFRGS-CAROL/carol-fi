b49f5fec1b8ff288d93608c8f39429d5dee1955e
Fernando <f_157@hotmail.com>
2018-08-27 17:03:48 -0300

    fi classes

--- fault_injector.py
+++ fault_injector.py
@@ -330,12 +330,12 @@ def gdb_inject_fault(**kwargs):
         # Search for block
         m = re.search("CUDA_BLOCK_FOCUS:.*block.*\((\d+),(\d+),(\d+)\).*", logging.search("CUDA_BLOCK_FOCUS"))
         if m:
-            block = [m.group(1), m.group(2), m.group(3)]
+            block = "{}_{}_{}".format(m.group(1), m.group(2), m.group(3))
 
         # Search for thread
-        m = re.search("CUDA_THREAD_FOCUS:.*thread.*\((\d+),(\d+),(\d+)\).*", logging.search("CUDA_BLOCK_FOCUS"))
+        m = re.search("CUDA_THREAD_FOCUS:.*thread.*\((\d+),(\d+),(\d+)\).*", logging.search("CUDA_THREAD_FOCUS"))
         if m:
-            thread = [m.group(1), m.group(2), m.group(3)]
+            thread = "{}_{}_{}".format(m.group(1), m.group(2), m.group(3))
 
         fi_successful = True
     except Exception as e:
