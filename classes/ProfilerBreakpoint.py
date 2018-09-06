9c84a45df86e51a9cb9da0c39278eb58334be253
Fernando <f_157@hotmail.com>
2018-09-06 12:14:06 -0300

    testing new approach

--- classes/ProfilerBreakpoint.py
+++ classes/ProfilerBreakpoint.py
@@ -46,6 +46,7 @@ class ProfilerBreakpoint(gdb.Breakpoint):
             else:
                 source_list.append([[l], assemble_lines[:]])
                 assemble_lines = []
+            print(l)
 
         last_not_zero_size = None
         ret_source = []
