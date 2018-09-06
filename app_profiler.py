70bb97b3031d255673ba1ef113cca28be8eb08d5
Fernando <f_157@hotmail.com>
2018-09-06 17:27:01 -0300

    testing new approach

--- app_profiler.py
+++ app_profiler.py
@@ -94,10 +94,10 @@ def main():
     generate_gold(conf=conf)
 
     # Load and re-save the kernel configuration txt file
-    kernel_list = cf.load_file(file_path=cp.KERNEL_INFO_DIR)
-    for kernel_dict in kernel_list:
-        kernel_dict['max_time'] = max_time_app
-    cf.save_file(file_path=cp.KERNEL_INFO_DIR, data=kernel_list)
+    # kernel_list = cf.load_file(file_path=cp.KERNEL_INFO_DIR)
+    # for kernel_dict in kernel_list:
+    #     kernel_dict['max_time'] = max_time_app
+    # cf.save_file(file_path=cp.KERNEL_INFO_DIR, data=kernel_list)
 
     print("1 - Profile finished\n###################################################")
 
