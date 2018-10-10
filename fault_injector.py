8f35006e301167da2ff5512113e9ec968628f4dd
Fernando <f_157@hotmail.com>
2018-10-10 09:32:44 -0300

    fault injector

--- fault_injector.py
+++ fault_injector.py
@@ -429,8 +429,9 @@ def bit_flip_selection(fault_model):
 
     # Least 16 bits
     elif fault_model == 4:
-        max_size_register_fault_model = 32
-        bits_to_flip[0] = random.randint(16, max_size_register_fault_model - 1)
+        # max_size_register_fault_model = 32
+        # bits_to_flip[0] = random.randint(16, max_size_register_fault_model - 1)
+        bits_to_flip[0] = random.randint(0, 16)
 
     # Least 8 bits
     elif fault_model == 5:
