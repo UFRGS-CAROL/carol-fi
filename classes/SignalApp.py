c79bb61f117aa829607a21138551a7696f3ed27f
Fernando <fernandofernandesantos@gmail.com>
2018-10-25 15:32:30 -0300

    flip value

--- classes/SignalApp.py
+++ classes/SignalApp.py
@@ -16,24 +16,24 @@ class SignalApp(Thread):
         super(SignalApp, self).__init__()
         self.__signal_cmd = signal_cmd
         self.__log = Logging(log_file=log_path, unique_id=unique_id)
-        self.__init_wait_time = uniform(float(max_wait_time) * cp.TIME_WAIT_START_SIGNAL,
-                                        float(max_wait_time) * cp.TIME_BEFORE_FIRST_SIGNAL)
+        self.__init_wait_time = uniform(0, float(max_wait_time) * cp.TIME_WAIT_START_SIGNAL)
         self.__signals_to_send = int(signals_to_send)
+        self.__time_to_sleep = (float(max_wait_time) - self.__init_wait_time) / float(self.__signals_to_send)
 
     def run(self):
-        # Sleep for a random time
-        time.sleep(self.__init_wait_time)
-
         # Send a series of signal to make sure gdb will flip a value in one of the interrupt signals
         log_string = "Sending a signal using command: {} after {}s.".format(self.__signal_cmd, self.__init_wait_time)
 
         if cp.DEBUG:
             print(log_string)
 
+        # Sleep for a random time
+        time.sleep(self.__init_wait_time)
+
         self.__log.info(log_string)
         for signals in range(0, self.__signals_to_send):
             os.system(self.__signal_cmd)
-            time.sleep(cp.TIME_TO_SLEEP)
+            time.sleep(self.__time_to_sleep)
 
     def get_int_wait_time(self):
         return self.__init_wait_time
