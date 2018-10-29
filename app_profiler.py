74adf2d74f9571a55b74baf8f1ce7623772d8277
Fernando <fernandofernandesantos@gmail.com>
2018-10-29 15:41:40 -0300

    app profiler

--- app_profiler.py
+++ app_profiler.py
@@ -1,9 +1,56 @@
 #!/usr/bin/python
 import argparse
 import os
+import sys
 import time
 import common_functions as cf
 import common_parameters as cp
+
+
+# helper function, if you do not want to use the picke function
+# def print_dictionary(d):
+#     print "{"
+#     for key in d:
+#         print "\t'" + key + "': " + d[key] + ","
+#     print "}"
+
+
+def generate_dict(sm_version, input_file_name):
+    if len(sys.argv) != 4:
+        print "Usage: python extract_reg_numbers.py <application name> <sm version> <stderr file name>"
+        print "Example: python extract_reg_numbers.py simple_add sm_35 stderr"
+        print "It is prefered that you run this script from the application directory and store the pickle file there."
+        sys.exit(-1)
+
+    f = open(input_file_name, "r")
+
+    # dictionary to store the number of allocated registers per static
+    kernel_reg = {}
+
+    kname = ""  # temporary variable to store the kname
+    check_for_regcount = False
+
+    # process the input file created by capturing the stderr while compiling the
+    # application using -Xptxas -v options
+    for line in f:  # for each line in the file
+        if "Compiling entry function" in line:  # if line has this string
+            kname = line.split("'")[1].strip()  # extract kernel name
+            check_for_regcount = True if sm_version in line else False
+        if check_for_regcount and ": Used" in line and "registers, " in line:
+            reg_num = line.split(':')[1].split()[1]  # extract register number
+            if kname not in kernel_reg:
+                kernel_reg[kname] = int(reg_num.strip())  # associate the extracted register number with the kernel name
+            else:
+                print "Warning: " + kname + " exists in the kernel_reg dictionary. Skipping this regcount."
+
+    # print the recorded kernel_reg dictionary
+    # pickle_filename = app + "_kernel_regcount.p"
+    # cPickle.dump(kernel_reg, open(pickle_filename, "wb"))
+    # print "Created the pickle file: " + os.getcwd() + "/" + pickle_filename
+    # print "Load it from the specific_params.py file"
+    return kernel_reg
+    # print_dictionary(kernel_reg)
+
 
 """
 Function that calls the profiler based on the injection mode
@@ -52,6 +99,9 @@ def main():
     os.system("rm -f {}".format(cp.KERNEL_INFO_DIR))
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--conf', dest="config_file", help='Configuration file', required=True)
+    parser.add_argument('-s', '--sm', dest="sm_processor", help='Stream multiprocessor arch', required=True)
+    parser.add_argument('-m', '--stderr', dest="make_stderr", help='Makefile stderr file', required=True)
+
 
     args = parser.parse_args()
 
@@ -72,9 +122,10 @@ def main():
     # saving gold
     generate_gold(conf=conf)
 
+    kernel_regs = generate_dict(sm_version=args.sm_processor, input_file_name=args.stderr)
     # Load and re-save the kernel configuration txt file
     # kernel_list = cf.load_file(file_path=cp.KERNEL_INFO_DIR)
-    cf.save_file(file_path=cp.KERNEL_INFO_DIR, data=[{'max_time': max_time_app}])
+    cf.save_file(file_path=cp.KERNEL_INFO_DIR, data=[{'max_time': max_time_app, 'kernel_registers': kernel_regs}])
 
     print("1 - Profile finished\n###################################################")
 
