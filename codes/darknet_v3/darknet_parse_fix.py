dd761d25c270346d6b815990dfc69ca95b472a3d
reliabilitylogdata <reliabilitylogdata@github.com>
2018-12-04 16:15:54 -0200

    foi

--- codes/darknet_v3/darknet_parse_fix.py
+++ codes/darknet_v3/darknet_parse_fix.py
@@ -2,55 +2,49 @@
 This parser will parse all information created on
 bug fixes on darknet
 """
-
+import csv
 import os
+import sys
 
-INDIR = "/tmp/double_fi/logs/{}"
-OUTDIR = "/tmp/double_fi/sdc_logs/{}"
+INDIR = sys.argv[1] + "/log/{}"
+CSV_FILE = sys.argv[2]
+OUTDIR = sys.argv[1] + "/sdc_logs/{}"
 
 
-def insert_sdc_in_errors(file_data):
-    sdc_not_mapped = []
-    i = 0
-    lines_mapped = 0
-    for line in file_data:
-        if 'SDC' in line:
-            lines_mapped += 1
-
-        try:
-            if 'ERR' in line and 'ERR' not in file_data[i + 1] and 'SDC' not in file_data[i + 1]:
-                sdc_not_mapped.append(i)
-        except:
-            sdc_not_mapped.append(i)
-
-        i += 1
-
+def insert_sdc_in_errors(fname, file_data):
+    lines_mapped = 6
     sdc_line = "#SDC Ite:{} KerTime:3.928865 AccTime:10.571838 KerErr:1 AccErr:1\n"
-    for ll in sdc_not_mapped:
-        lines_mapped += 1
-        file_data.insert(ll + 1, sdc_line.format(lines_mapped))
+
+    for i, line in enumerate(file_data):
+        if 'ERR' in line:
+            try:
+                if ('IT' in file_data[i + 1] or 'END' in file_data[i + 1]) and 'SDC' not in file_data[i + 1]:
+                    lines_mapped += 1
+                    file_data.insert(i + 1, sdc_line.format(lines_mapped))
+            except:
+                print fname
+                lines_mapped += 1
+                file_data.insert(i + 1, sdc_line.format(lines_mapped))
 
     return file_data
 
-
-all_files = [f for f in os.listdir(INDIR.format(''))]
+with open(CSV_FILE, "r") as fp:
+    reader = csv.DictReader(fp)
+    all_files = [f['log_file'] for f in reader]
 
 for log_file in all_files:
     with open(INDIR.format(log_file), "r") as fp:
         file_data_full = fp.readlines()
 
-    err_in_file = False
-    sdc_in_file = False
-    for dt in file_data_full:
-        if 'ERR' in dt:
-            err_in_file = True
+    check_file = False
+    for ll in file_data_full:
+        if 'ERR' in ll:
+            check_file = True
+            break
+    output = file_data_full
+    # wrong file
+    if check_file:
+        output = insert_sdc_in_errors(fname=log_file, file_data=file_data_full)
 
-        if 'SDC' in dt:
-            sdc_in_file = True
-
-    if sdc_in_file is False and err_in_file is True:
-        # wrong file
-        output = insert_sdc_in_errors(file_data=file_data_full)
-
-        with open(OUTDIR.format(log_file), "w") as fo:
-            fo.writelines(output)
+    with open(OUTDIR.format(log_file), "w") as fo:
+        fo.writelines(output)
