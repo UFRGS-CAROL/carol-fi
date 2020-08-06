672fc599243cc654b47b5778dabb73e0dd247c9d
Your Name <you@example.com>
2020-08-06 01:12:06 -0300

    Update on parse log file and fault injector correct message

--- scripts/parse_log_file.py
+++ scripts/parse_log_file.py
@@ -4,6 +4,7 @@ CAROL-FI CUDA
 """
 
 from sys import argv
+import pandas as pd
 
 MAX_REGISTER_NUMBER = 255
 
@@ -14,8 +15,27 @@ def main():
     :param: none
     :return: none
     """
-    assert len(argv) < 3, "usage: {}.py <csv_path>".format(argv[0])
+    assert len(argv) >= 2, "usage: {}.py <csv_path> [output path for xlsx optional]".format(argv[0])
     csv_path = argv[1]
+    avf_log_df = pd.read_csv(csv_path)
+    histogram = avf_log_df[['sdc', 'hang', 'crash', 'register', 'instruction']]
+    inst_histogram = histogram['instruction'].value_counts(normalize=True)
+    rf_histogram = histogram['register'].value_counts(normalize=True)
+    avf = histogram[['sdc', 'hang', 'crash']].astype(int)
+    avf['sdc'] = avf['sdc'].value_counts()
+    avf['hang'] = avf['hang'].value_counts()
+    avf['crash'] = avf['crash'].value_counts()
+    avf = avf.dropna(how='all')
+    avf = avf.transpose().rename(columns={0: "Negative", 1: "Positive"})
+
+    output_path = 'output.xlsx'
+    if len(argv) == 3:
+        output_path = argv[2]
+
+    with pd.ExcelWriter(output_path) as writer:
+        inst_histogram.to_excel(writer, sheet_name='INST histogram')
+        rf_histogram.to_excel(writer, sheet_name='RF histogram')
+        avf.to_excel(writer, sheet_name='AVF')
 
 
 if __name__ == '__main__':
